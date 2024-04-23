cf24d9454797c406796c6d2414723c99c603dc4d
skylor burris <s_burris@mail.fhsu.edu>
2024-04-23 17:57:44 -0500

    updated script start cors options

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,22 +1,6 @@
-// Cross-Origin Resource Sharing
-const whiteList = [
-    "https://www.google.com",
-    "http://127.0.0.1:5500",
-    "http://localhost:3000",
-    "https://kayscrochetmobileapp-5c1e1888702b.herokuapp.com",
-    "https://www.kayscrochet.us",
-    "https://kayscrochetapp-e13180bf49a3.herokuapp.com/",
-    "https://youthful-skitter-snake.glitch.me/",
-    "https://incredible-scythe-mouse.glitch.me/"
-  ];
-  
-  const corsOptions = {
-    origin: (origin, callback) => {
-      if (whiteList.indexOf(origin) !== -1 || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    optionsSuccessStatus: 200,
-  };
+const corsOptions = {
+  origin: (origin, callback) => {
+    callback(null, true);  
+  },
+  optionsSuccessStatus: 200,
+};
