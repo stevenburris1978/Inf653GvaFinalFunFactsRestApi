3b914ad29b9ab6f9c90f899cd8825860b1c55384
skylor burris <s_burris@mail.fhsu.edu>
2024-04-23 18:52:21 -0500

    update

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,6 +1,20 @@
-const corsOptions = {
-  origin: (origin, callback) => {
-    callback(null, true);  
-  },
-  optionsSuccessStatus: 200,
-};
+// Cross-Origin Resource Sharing
+const whiteList = [
+    "https://www.google.com",
+    "http://127.0.0.1:5500",
+    "http://localhost:3000",
+    "https://kayscrochetmobileapp-5c1e1888702b.herokuapp.com",
+    "https://www.kayscrochet.us",
+    "https://kayscrochetapp-e13180bf49a3.herokuapp.com/"
+  ];
+  
+  const corsOptions = {
+    origin: (origin, callback) => {
+      if (whiteList.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    optionsSuccessStatus: 200,
+  };
